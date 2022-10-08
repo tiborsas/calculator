a9bf66801512d4053392aca5ba7d33a950a6f84b
tibor <tibor_sas@yahoo.com>
2022-10-08 08:10:14 +0200

    Adding a method to Calculator.java class

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+"public class Calculator {}" 
+"This project will hold a simple implementation of a Calcualtor in Java" 
+public class Calculator() {
+    public static void main(String[] args) {}
+}
+public class Calculator {
+    public int addition(int a, int b) {
+      return a + b;
+     }
+ }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
  "readme" 
+"# Calculator Project" 
